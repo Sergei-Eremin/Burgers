8b19284063c7685d149ebee07d3664ec1f3a3023
ViniPuh <serezhanv@gmail.com>
2020-03-30 16:26:01 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -39,17 +39,22 @@ sectionMenu.addEventListener("click", function(event){
     let eventClick = event.target.tagName.toLowerCase()
 
     if (eventClick === "a" || eventClick === "span") {
-        for (let y = 0; y < menuAccordion__item.length; y++) {
-            menuAccordion__item[y].classList.remove("menu-accordion__item_active")
-        };
         let clickTarget = event.target.closest(".menu-accordion__item")
-        // console.log(target);
-        clickTarget.classList.add("menu-accordion__item_active")
+
+        if (!event.target.closest("li").classList.contains("menu-accordion__item_active")) {
+            for (let y = 0; y < menuAccordion__item.length; y++) {
+                menuAccordion__item[y].classList.remove("menu-accordion__item_active")
+            };
+            // console.log(target);
+            clickTarget.classList.add("menu-accordion__item_active")
+        } else {
+            clickTarget.classList.remove("menu-accordion__item_active")
+        }
     } 
 
 })
 
-// 
+// accordion
 let teamPeople__wrapper = document.querySelector(".team-people__wrapper");
 
 teamPeople__wrapper.addEventListener('click', function(event){
@@ -59,8 +64,8 @@ teamPeople__wrapper.addEventListener('click', function(event){
 
     let eventClick = event.target.tagName.toLowerCase();
 
+    // адаптивный по высоте
     if (eventClick === 'li' || eventClick === 'a') {
-        
         for (let i = 0; i < teamMember.length; i++) {
             teamMember[i].lastElementChild.style.maxHeight = "0px"
             teamMember[i].classList.remove("team-member_active")
@@ -72,21 +77,118 @@ teamPeople__wrapper.addEventListener('click', function(event){
         
         let clickTarget = event.target.closest('.team-member');
         clickTarget.classList.add('team-member_active');
-
-
-        // БЕЗ АДАПТИВА
-        // for (let i = 0; i < teamMember.length; i++) {
-        //     teamMember[i].classList.remove("team-member_active")
-        //     console.log(teamMember.length);
-        // };
-
-        // let clickTarget = event.target.closest('.team-member');
-        // clickTarget.classList.add('team-member_active')
-
     }
 })
 
-
-
 // slider
 
+function Slider (selector, settings){
+
+    this.target = document.querySelector(selector);
+
+    let sliderItems = this.target.querySelector("ul");
+
+    this.amountSlides = sliderItems.children.length;
+
+    this.currentIndexSlide = 0;
+    console.log(this);
+    
+    if(settings){
+
+        if(settings.animate) {
+            sliderItems.style.transitionTimingFunction = settings.animate;
+        }
+
+        if(settings.duration) {
+            sliderItems.style.transitionDuration = settings.duration + "s";
+        }
+    };
+
+    this.target.addEventListener("click",(event) => {
+        console.log(event.target.closest('[data-vector]'));
+        let arrowLink = event.target.closest('[data-vector]');
+        event.preventDefault();
+
+        if(arrowLink) {
+            let vector = arrowLink.dataset.vector;
+            this[vector]();
+        }
+    });
+
+    this.next = function(){
+        if(this.currentIndexSlide < this.amountSlides -1){
+            console.log("next slide")
+            this.currentIndexSlide++
+        } else {
+            this.currentIndexSlide = 0;
+        };
+        translateSlider(this.currentIndexSlide)
+    };
+
+    this.back = function(){
+        if(this.currentIndexSlide > 0){
+            console.log("back slide")
+            this.currentIndexSlide--
+            translateSlider(this.currentIndexSlide)
+        };
+    };
+    
+    function translateSlider(index) {
+        sliderItems.style.transform = `translateX(${-(index * 100)}%)`
+    };
+
+};
+
+const SLIDER_1 = new Slider("#dark-burger-slider",{
+    animate: "cubic-bezier(.52,.66,.65,.72)",
+    duration: .2
+});
+
+console.log(SLIDER_1);
+
+// delivery
+
+let deliveryForm = document.querySelector('.delivery-form');
+let deliverySubmitBtn = document.querySelector('.delivery-form__btn_submit');
+
+
+deliveryForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    if(validateForm(deliveryForm)){
+        const xhr = new XMLHttpRequest();
+        let formData = new FormData(deliveryForm);
+        xhr.responseType = 'json';
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send();
+        console.log(formData);
+        
+        
+    }
+});
+
+function validateForm(form) {
+    let valid = true;
+    
+    if(!validateField(form.elements.clientName)){
+        valid = false;
+    }
+
+    if(!validateField(form.elements.clientPhone)){
+        valid = false;
+    }
+
+    if(!validateField(form.elements.clientComment)){
+        valid = false;
+    }
+
+    return valid;
+};
+
+function validateField(field){
+    if(!field.checkValidity()){
+        return false;
+    } else {
+        return true;
+    }
+};
